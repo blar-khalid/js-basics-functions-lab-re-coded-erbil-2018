496f1cc7a8a2d683319d5f69e1448ca510c4a65c
blar khalid <blarharki@gmail.com>
2018-09-04 07:55:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+  if(block>42)
+  {
+  return block -  42;
+
+  }else{
+    return 42 - block;
+  }
+  
+  }
+  
+  
+  
+function distanceFromHqInFeet (blockNumber) {
+    if(blockNumber>42)
+  {
+  return (blockNumber -  42) * 264;
+
+  }else{
+    return (42 - blockNumber) * 264;
+  }
+}
+
+function distanceTravelledInFeet(a ,b){
+    if(b>=a)
+  {
+  return (b-a) * 264;
+
+  }else{
+    return (a-b) * 264;
+  }
+}
+
+
+function calculatesFarePrice(a,b){
+
+  
+  if( distanceTravelledInFeet(a ,b) <=400){
+    return 0;
+  }
+  else if( distanceTravelledInFeet(a ,b) > 400 &&  distanceTravelledInFeet(a ,b)<= 2000){
+    return .02 * distanceTravelledInFeet(a ,b);
+  }else if ( distanceTravelledInFeet(a ,b) > 2000 && distanceTravelledInFeet(a ,b) < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
+
